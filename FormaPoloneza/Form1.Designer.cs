69543bd9a2ce7922d024dc48e37e9ff2b030e1c0
ralucaC117 <58273375+ralucaC117@users.noreply.github.com>
2021-03-12 14:51:53 +0200

    project added

---
+++ FormaPoloneza/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace FormaPoloneza
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox_expr = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_x = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox_expr
+            // 
+            this.textBox_expr.Location = new System.Drawing.Point(419, 80);
+            this.textBox_expr.Name = "textBox_expr";
+            this.textBox_expr.Size = new System.Drawing.Size(229, 22);
+            this.textBox_expr.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(519, 234);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(183, 86);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(96, 391);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Rezultat:";
+            // 
+            // textBox_x
+            // 
+            this.textBox_x.Location = new System.Drawing.Point(419, 126);
+            this.textBox_x.Name = "textBox_x";
+            this.textBox_x.Size = new System.Drawing.Size(213, 22);
+            this.textBox_x.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(351, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Expresie";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(377, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "X";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_x);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox_expr);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_expr;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_x;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ FormaPoloneza/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormaPoloneza
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int PrioritateOp(char c)
+        {
+            switch (c)
+            {
+                case '+': return 1;
+                case '-': return 1;
+                case '*': return 2;
+                case '/': return 2;
+                case '^': return 3;
+                default: return 0;
+
+            }
+
+        }
+        public bool IsParanteza(char c)
+        {
+            if (c == '(' || c == ')')
+                return true;
+            return false;
+
+        }
+        public String FormaPoloneza(String Expresie)
+        {
+
+            int l = Expresie.Length;
+            int i = 0;
+            Stack<Char> Operatori = new Stack<Char>();
+            StringBuilder FP = new StringBuilder();
+            while (i < l)
+            {
+                if (Expresie[i] == '(')
+                {
+                    Operatori.Push('(');
+                    i++;
+                }
+                else
+                if (Expresie[i] == ')')
+                {
+                    while (Operatori.Count > 0 && Operatori.Peek() != '(')        
+                        FP.Append(Operatori.Pop());      
+                    i++;
+                    Operatori.Pop();
+                }
+                else
+                if (PrioritateOp(Expresie[i]) == 0)
+                {
+                    while (PrioritateOp(Expresie[i]) == 0 && Expresie[i]!=')' && Expresie[i]!='(')
+                    {
+                        FP.Append(Expresie[i]);
+                        i++;
+                        if (i == l)
+                            break;
+                    }
+                    
+                    FP.Append(' ');
+
+                }
+                else
+                if (PrioritateOp(Expresie[i]) != 0)
+                {
+                    while (Operatori.Count > 0 && (char)Operatori.Peek() != '(' && PrioritateOp(Expresie[i]) < PrioritateOp((char)Operatori.Peek()))
+                    { 
+                        char k = (char)Operatori.Pop();
+                        FP.Append(k);
+                    }
+                   
+                    Operatori.Push(Expresie[i]);
+                    i++;
+                }
+            }
+            while (Operatori.Count > 0)
+                FP.Append(Operatori.Pop());
+            return FP.ToString();
+
+        }
+        public double Ordonata(String Expresie, double x)
+        {
+            String Fun = FormaPoloneza(Expresie);
+            Stack<Double> Functie = new Stack<double>();
+            int n = Fun.Length;
+            for(int i=0; i<n;)
+            {
+                if (Char.IsDigit(Fun[i]) == true)
+                {
+                    double nr = 0;
+                    while ((Char.IsDigit(Fun[i]) == true))
+                    {
+                        nr = nr * 10 + Fun[i] - '0';
+                        i++;
+                    }
+                    Functie.Push(nr);
+                }
+
+                else
+                if (Fun[i] == 'x')
+                {
+                    Functie.Push(x);
+                    i++;
+                }
+
+                else
+                if (Fun[i] == ' ')
+                    i++;
+
+                else
+                if(PrioritateOp(Fun[i])!=0 && Fun[i]!='x' && Char.IsDigit(Fun[i])==false)
+                {
+                    if(Fun[i]=='^')
+                    {
+                        double t1, t2;
+                        t1 = Functie.Pop();
+                        t2 = Functie.Pop();
+                        Functie.Push(Math.Pow(t2,t1));
+                    }
+                    else 
+                    if(Fun[i]=='+')
+                    {
+                        double t1, t2;
+                        t1 = Functie.Pop();
+                        t2 = Functie.Pop();
+                        Functie.Push(t1+t2);
+                    }
+                    else
+                    if(Fun[i] == '-')
+                    {
+                        double t1, t2;
+                        t1 = Functie.Pop();
+                        t2 = Functie.Pop();
+                        Functie.Push(t2-t1);
+                    }
+                    else
+                    if(Fun[i] == '*')
+                    {
+                        double t1, t2;
+                        t1 = Functie.Pop();
+                        t2 = Functie.Pop();
+                        Functie.Push(t2*t1);
+                    }
+                    else
+                    if (Fun[i] == '/')
+                    {
+                        double t1, t2;
+                        t1 = Functie.Pop();
+                        t2 = Functie.Pop();
+                        Functie.Push((double)t2/t1);
+                    }
+                    i++;
+                }
+            }
+            double rezultat = Functie.Pop();
+            return rezultat;   
+        }
+            
+  
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String Expr = textBox_expr.Text.ToString();
+            String Rezultat = FormaPoloneza(Expr);
+            // label1.Text = Rezultat;
+            double x = Convert.ToDouble(textBox_x.Text);
+            double rez = Ordonata(Expr, x);
+            label1.Text = rez.ToString();
+
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
